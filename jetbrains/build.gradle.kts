8660c6ea97013e3af11f504d8b127a5fda531b51
gaoxiao <gaoxiao6331@163.com>
2026-03-11 18:34:25 +0800

    chore(jetbrains): add logo

--- jetbrains/build.gradle.kts
+++ jetbrains/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
     kotlin("plugin.serialization") version "2.1.0"
 }
 
-group = "com.deplens"
-version = "0.1.0"
+group = "deplens"
+version = "0.1.1"
 
 repositories {
     mavenCentral()

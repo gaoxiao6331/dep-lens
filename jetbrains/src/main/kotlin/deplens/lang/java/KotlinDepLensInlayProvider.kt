8273f1e56e0bad8804df2665391a19c29cf7c5fc
gaoxiao <gaoxiao6331@163.com>
2026-03-24 21:19:59 +0800

    feat(jetbrains): update maven resolver

--- jetbrains/src/main/kotlin/deplens/lang/java/KotlinDepLensInlayProvider.kt
+++ jetbrains/src/main/kotlin/deplens/lang/java/KotlinDepLensInlayProvider.kt
@@ -8,6 +8,7 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.DumbAware
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiMember
 import deplens.utils.inlay.GithubInlayUtils
 import deplens.utils.resolver.MavenRepoResolver
 import org.jetbrains.kotlin.psi.KtFile
@@ -33,7 +34,18 @@ class KotlinDepLensInlayProvider : InlayHintsProvider, DumbAware {
             override fun collectFromElement(element: PsiElement, sink: InlayTreeSink) {
                 if (element !is KtImportDirective) return
 
-                val repoKey = MavenRepoResolver.repoKeyFromImport(element.importPath?.pathStr) ?: return
+                val resolved = element.importedReference
+                    ?.references
+                    ?.firstNotNullOfOrNull { it.resolve() }
+
+                val resolvedClass = when (resolved) {
+                    is com.intellij.psi.PsiClass -> resolved
+                    is PsiMember -> resolved.containingClass
+                    else -> null
+                }
+
+                val resolvedPath = resolvedClass?.containingFile?.virtualFile?.path
+                val repoKey = MavenRepoResolver.repoKeyFromResolvedPath(resolvedPath) ?: return
                 val offset = element.textRange.endOffset
 
                 GithubInlayUtils.addRepoInlay(file, sink, repoKey, offset)
